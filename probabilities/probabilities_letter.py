3e222ff423778a8001209b28e90d8627209f218f
Gametoy20 <92245607+Gametoy20@users.noreply.github.com>
2024-06-17 19:36:09 +0700

    Add files via upload

---
+++ probabilities/probabilities_letter.py
@@ -0,0 +1,18 @@
+import json
+import math
+
+letters = ["*", 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+           'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+probabilities = []
+for x in range(0, len(letters)):
+    probabilities.append(math.exp(0.4 * x))
+
+total = sum(probabilities)
+probabilities = [p / total for p in probabilities]
+
+# Sauvegarde des probabilités dans un fichier JSON
+with open('probabilities_letter.json', 'w') as f:
+    json.dump(probabilities, f)
+
+print("Probabilités sauvegardées avec succès.")
